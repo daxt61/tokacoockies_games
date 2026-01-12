99fb7c12f9bfb63f8ff686de20d4ad10fb600905
daxt61 <bene.gesserit1@gmail.com>
2026-01-12 18:33:26 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,26 +1,22 @@
-import os
-import uuid
+import os, uuid
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit, join_room, leave_room
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'sorek_secret_key'
-
-# Configuration Socket.io pour Render (mode eventlet)
+# Configuration async_mode='eventlet' est CRUCIALE pour Render
 socketio = SocketIO(app, cors_allowed_origins="*", async_mode='eventlet', max_http_buffer_size=1e7)
 
-# Stockage des données en mémoire
 users = {} 
 shifumi_data = {} 
 
 @app.route('/')
 def index():
-    # Cherche automatiquement le fichier index.html dans le dossier /templates
+    # C'est LA ligne qui corrige ton erreur "FileNotFound"
     return render_template('index.html')
 
 @socketio.on('join')
 def on_join(data):
-    # request.sid est l'identifiant unique de la connexion du joueur
     users[request.sid] = {
         'pseudo': data.get('pseudo', 'Anonyme'), 
         'activity': '🏠 Accueil', 
@@ -31,9 +27,13 @@ def on_join(data):
 @socketio.on('disconnect')
 def on_disc():
     if request.sid in users:
-        user_room = users[request.sid]['room']
-        if user_room:
-            emit('fin_duel', room=user_room)
+        rid = users[request.sid]['room']
+        if rid:
+            # Si le joueur était en jeu, on prévient l'adversaire
+            emit('fin_duel', room=rid)
+            # On nettoie les données Shifumi si elles existent
+            if rid in shifumi_data:
+                del shifumi_data[rid]
         del users[request.sid]
         emit('update_users', users, broadcast=True)
 
@@ -53,7 +53,6 @@ def accept(data):
     gtype = data['game']
     
     if p1 in users and p2 in users:
-        # Création d'une salle unique pour le duel
         rid = f"room_{uuid.uuid4().hex[:6]}"
         join_room(rid, sid=p1)
         join_room(rid, sid=p2)
@@ -64,27 +63,28 @@ def accept(data):
         if gtype == "Shifumi": 
             shifumi_data[rid] = {}
             
+        # On lance le jeu pour les deux
         emit('start_duel', {'room': rid, 'game': gtype, 'opp': users[p2]['pseudo'], 'turn': True, 'sym': 'X'}, room=p1)
         emit('start_duel', {'room': rid, 'game': gtype, 'opp': users[p1]['pseudo'], 'turn': False, 'sym': 'O'}, room=p2)
         emit('update_users', users, broadcast=True)
 
-@socketio.on('msg')
-def msg(data):
-    if request.sid in users:
-        emit('new_msg', {'p': users[request.sid]['pseudo'], 'm': data['m']}, broadcast=True)
-
-@socketio.on('draw_data')
-def draw(data):
-    # Envoie les coordonnées du dessin à tous les autres joueurs
-    emit('draw_remote', data, broadcast=True, include_self=False)
-
-@socketio.on('clear_canvas')
-def clear_canvas():
-    emit('canvas_cleared', broadcast=True, include_self=False)
-
 @socketio.on('coup_morpion')
 def coup_m(data):
     emit('receive_move', data, room=data['room'], include_self=False)
+
+@socketio.on('coup_shifumi')
+def coup_s(data):
+    rid, move = data['room'], data['move']
+    if rid in shifumi_data:
+        shifumi_data[rid][request.sid] = move
+        # Si les deux ont joué
+        if len(shifumi_data[rid]) == 2:
+            p_ids = list(shifumi_data[rid].keys())
+            emit('resultat_shifumi', {
+                'p1': p_ids[0], 'm1': shifumi_data[rid][p_ids[0]], 
+                'p2': p_ids[1], 'm2': shifumi_data[rid][p_ids[1]]
+            }, room=rid)
+            shifumi_data[rid] = {} # Reset pour la prochaine manche
 
 @socketio.on('quitter_duel')
 def quit_d(data):
@@ -95,9 +95,23 @@ def quit_d(data):
             if users[sid]['room'] == rid:
                 users[sid]['room'] = None
                 users[sid]['activity'] = '🏠 Accueil'
+                leave_room(rid, sid=sid)
+        if rid in shifumi_data: del shifumi_data[rid]
         emit('update_users', users, broadcast=True)
 
+@socketio.on('msg')
+def msg(data):
+    if request.sid in users:
+        emit('new_msg', {'p': users[request.sid]['pseudo'], 'm': data['m']}, broadcast=True)
+
+@socketio.on('draw_data')
+def draw(data):
+    emit('draw_remote', data, broadcast=True, include_self=False)
+
+@socketio.on('clear_canvas')
+def clear_canvas():
+    emit('canvas_cleared', broadcast=True, include_self=False)
+
 if __name__ == '__main__':
-    # Utilisation du port fourni par Render
     port = int(os.environ.get("PORT", 5000))
     socketio.run(app, host='0.0.0.0', port=port)
