f3071a949284bb366b45d6f2744177f94f39f348
daxt61 <bene.gesserit1@gmail.com>
2026-01-13 17:58:47 +0100

    Update server.py

--- server.py
+++ server.py
@@ -11,8 +11,8 @@ bcrypt = Bcrypt(app)
 socketio = SocketIO(app, cors_allowed_origins="*", async_mode='eventlet')
 
 # --- CONFIGURATION SUPABASE ---
-SUPABASE_URL = "https://TON_ID.supabase.co"
-SUPABASE_KEY = "TA_CLE_SERVICE_ROLE"
+SUPABASE_URL = "https://rzzhkdzjnjeeoqbtlles.supabase.co"
+SUPABASE_KEY = "9MjZLGabGOUWEZuo"
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 connected_users = {} # sid: {pseudo, mult, guild}
@@ -132,3 +132,4 @@ def disc():
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+
