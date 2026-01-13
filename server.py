9871f8ad8e4aab9d07d481c6767a140dc0f4462c
daxt61 <bene.gesserit1@gmail.com>
2026-01-13 17:31:02 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,117 +1,143 @@
-import os, uuid
+import os, uuid, random
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit, join_room, leave_room
 from flask_bcrypt import Bcrypt
 from supabase import create_client, Client
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'sorek_cloud_2026_final'
+app.config['SECRET_KEY'] = 'sorek_v5_mega'
 bcrypt = Bcrypt(app)
 socketio = SocketIO(app, cors_allowed_origins="*", async_mode='eventlet')
 
-# --- CONFIGURATION SUPABASE ---
-SUPABASE_URL = "https://rzzhkdzjnjeeoqbtlles.supabase.co"
-SUPABASE_KEY = "9MjZLGabGOUWEZuo"
+# CONFIG SUPABASE
+SUPABASE_URL = "https://TON_ID.supabase.co"
+SUPABASE_KEY = "TA_CLE_SERVICE_ROLE"
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
-connected_users = {} # sid: {pseudo, mult, room}
+connected_users = {} # sid: {pseudo, mult, guild}
+pending_requests = {} # leader_pseudo: [list_of_applicants]
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def index(): return render_template('index.html')
 
-# --- AUTHENTIFICATION ---
 @socketio.on('login_action')
 def auth_logic(data):
-    p, pwd, type_auth = data['pseudo'].strip(), data['password'], data['type']
+    p, pwd, t = data['pseudo'].strip(), data['password'], data['type']
     res = supabase.table("users").select("*").eq("pseudo", p).execute()
     user = res.data[0] if res.data else None
 
-    if type_auth == 'register':
-        if user: return emit('auth_error', "Ce pseudo existe déjà !")
-        hash_pw = bcrypt.generate_password_hash(pwd).decode('utf-8')
-        supabase.table("users").insert({"pseudo": p, "password": hash_pw}).execute()
-        res = supabase.table("users").select("*").eq("pseudo", p).execute()
-        user = res.data[0]
+    if t == 'register':
+        if user: return emit('auth_error', "Pseudo pris !")
+        h = bcrypt.generate_password_hash(pwd).decode('utf-8')
+        supabase.table("users").insert({"pseudo": p, "password": h}).execute()
+        user = supabase.table("users").select("*").eq("pseudo", p).execute().data[0]
 
     if user and bcrypt.check_password_hash(user['password'], pwd):
-        connected_users[request.sid] = {'pseudo': p, 'mult': user['multiplier'], 'room': None}
-        emit('login_ok', {'pseudo': p, 'clicks': user['clicks'], 'mult': user['multiplier']})
+        connected_users[request.sid] = {'pseudo': p, 'mult': user['multiplier'], 'guild': user.get('guild_name')}
+        emit('login_ok', {'pseudo': p, 'clicks': user['clicks'], 'mult': user['multiplier'], 'guild': user.get('guild_name')})
+        
+        # Vérifier si des gens veulent join sa guilde
+        if p in pending_requests and pending_requests[p]:
+            for applicant in pending_requests[p]:
+                emit('guild_request', {'from': applicant}, room=request.sid)
+        
         broadcast_global_data()
-    else:
-        emit('auth_error', "Identifiants invalides.")
+    else: emit('auth_error', "Erreur login")
 
-# --- CLICKER & LEADERBOARD ---
+# --- CLICKER & GUILD LOGIC ---
 @socketio.on('add_click')
 def add_click():
-    if request.sid in connected_users:
-        u = connected_users[request.sid]
+    sid = request.sid
+    if sid in connected_users:
+        u = connected_users[sid]
+        # Update User
         res = supabase.table("users").select("clicks").eq("pseudo", u['pseudo']).execute()
         new_total = res.data[0]['clicks'] + u['mult']
         supabase.table("users").update({"clicks": new_total}).eq("pseudo", u['pseudo']).execute()
+        
+        # Update Guild si existe
+        if u['guild']:
+            g_res = supabase.table("guilds").select("total_clicks").eq("name", u['guild']).execute()
+            if g_res.data:
+                new_g_total = g_res.data[0]['total_clicks'] + u['mult']
+                supabase.table("guilds").update({"total_clicks": new_g_total}).eq("name", u['guild']).execute()
+        
         emit('update_score', {'clicks': new_total})
-        broadcast_leaderboard()
+        send_relative_lb(u['pseudo'])
 
-@socketio.on('buy_upgrade')
-def buy_up():
-    if request.sid in connected_users:
-        p = connected_users[request.sid]['pseudo']
-        res = supabase.table("users").select("clicks", "multiplier").eq("pseudo", p).execute()
-        u_data = res.data[0]
-        cost = u_data['multiplier'] * 100
-        if u_data['clicks'] >= cost:
-            new_mult = u_data['multiplier'] + 1
-            new_clicks = u_data['clicks'] - cost
-            supabase.table("users").update({"clicks": new_clicks, "multiplier": new_mult}).eq("pseudo", p).execute()
-            connected_users[request.sid]['mult'] = new_mult
-            emit('login_ok', {'pseudo': p, 'clicks': new_clicks, 'mult': new_mult})
-            broadcast_leaderboard()
+@socketio.on('create_guild')
+def create_g(data):
+    p = connected_users[request.sid]['pseudo']
+    name = data['name'].strip()
+    try:
+        supabase.table("guilds").insert({"name": name, "leader": p}).execute()
+        supabase.table("users").update({"guild_name": name}).eq("pseudo", p).execute()
+        connected_users[request.sid]['guild'] = name
+        emit('guild_update', {'guild': name})
+        broadcast_global_data()
+    except: emit('notif', "Erreur : Nom déjà pris ou tu as déjà une guilde.")
 
-def broadcast_leaderboard():
-    res = supabase.table("users").select("pseudo", "clicks").order("clicks", desc=True).limit(10).execute()
-    emit('update_leaderboard', {'lb': [{"p": r['pseudo'], "c": r['clicks']} for r in res.data]}, broadcast=True)
+@socketio.on('join_guild_request')
+def join_req(data):
+    p = connected_users[request.sid]['pseudo']
+    target_guild = data['guild']
+    res = supabase.table("guilds").select("leader").eq("name", target_guild).execute()
+    if res.data:
+        leader = res.data[0]['leader']
+        if leader not in pending_requests: pending_requests[leader] = []
+        pending_requests[leader].append(p)
+        # Si leader est co, on le prévient
+        for sid, info in connected_users.items():
+            if info['pseudo'] == leader:
+                emit('guild_request', {'from': p}, room=sid)
+        emit('notif', "Demande envoyée au chef !")
+
+@socketio.on('answer_guild')
+def answer(data):
+    leader = connected_users[request.sid]['pseudo']
+    applicant = data['user']
+    if data['accept']:
+        g_res = supabase.table("guilds").select("name").eq("leader", leader).execute()
+        g_name = g_res.data[0]['name']
+        supabase.table("users").update({"guild_name": g_name}).eq("pseudo", applicant).execute()
+        emit('notif', f"Tu as rejoint la guilde {g_name}", room=get_sid(applicant))
+    if leader in pending_requests:
+        pending_requests[leader] = [x for x in pending_requests[leader] if x != applicant]
+
+@socketio.on('trahir_guilde')
+def trahir():
+    p = connected_users[request.sid]['pseudo']
+    supabase.table("users").update({"guild_name": None, "clicks": 0, "multiplier": 1}).eq("pseudo", p).execute()
+    connected_users[request.sid]['guild'] = None
+    connected_users[request.sid]['mult'] = 1
+    emit('login_ok', {'pseudo': p, 'clicks': 0, 'mult': 1, 'guild': None})
+    emit('notif', "Tu as tout perdu en trahissant ta guilde...")
+
+# --- CLASSEMENTS ---
+def send_relative_lb(pseudo):
+    # Récupérer tout le monde trié par clics
+    res = supabase.table("users").select("pseudo", "clicks").order("clicks", desc=True).execute()
+    all_users = res.data
+    idx = next((i for i, item in enumerate(all_users) if item["pseudo"] == pseudo), -1)
+    
+    start = max(0, idx - 5)
+    end = min(len(all_users), idx + 6)
+    emit('relative_lb', {'lb': all_users[start:end], 'my_pos': idx + 1})
+
+@socketio.on('get_full_lb')
+def full_lb():
+    u_res = supabase.table("users").select("pseudo", "clicks").order("clicks", desc=True).limit(20).execute()
+    g_res = supabase.table("guilds").select("name", "total_clicks").order("total_clicks", desc=True).limit(20).execute()
+    emit('full_lb_data', {'users': u_res.data, 'guilds': g_res.data})
 
 def broadcast_global_data():
-    broadcast_leaderboard()
     users_list = {sid: u['pseudo'] for sid, u in connected_users.items()}
     emit('update_users_list', {'users': users_list}, broadcast=True)
 
-# --- CHAT & JEUX (CORRIGÉ SANS RELOAD) ---
-@socketio.on('msg')
-def chat(data):
-    if request.sid in connected_users:
-        emit('new_msg', {'p': connected_users[request.sid]['pseudo'], 'm': data['m']}, broadcast=True)
-
-@socketio.on('send_defi')
-def defi(data):
-    if data['target_sid'] in connected_users:
-        emit('receive_defi', {'from_sid': request.sid, 'from_name': connected_users[request.sid]['pseudo']}, room=data['target_sid'])
-
-@socketio.on('accept_defi')
-def accept(data):
-    p1, p2 = data['opp_sid'], request.sid
-    rid = f"room_{uuid.uuid4().hex[:6]}"
-    join_room(rid, sid=p1); join_room(rid, sid=p2)
-    connected_users[p1]['room'] = rid; connected_users[p2]['room'] = rid
-    emit('start_game', {'room': rid, 'opp': connected_users[p2]['pseudo'], 'turn': True}, room=p1)
-    emit('start_game', {'room': rid, 'opp': connected_users[p1]['pseudo'], 'turn': False}, room=p2)
-
-@socketio.on('game_move')
-def g_move(data):
-    emit('opp_move', data, room=data['room'], include_self=False)
-
-@socketio.on('quitter_jeu')
-def quitter(data):
-    rid = data.get('room')
-    if rid:
-        emit('fin_duel', room=rid)
-        leave_room(rid)
-
-@socketio.on('disconnect')
-def disc():
-    if request.sid in connected_users:
-        del connected_users[request.sid]
-        broadcast_global_data()
+def get_sid(pseudo):
+    for s, i in connected_users.items():
+        if i['pseudo'] == pseudo: return s
+    return None
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
